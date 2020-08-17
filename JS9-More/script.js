2cbb145f568597c343bd5db332169e804d70e62c
Sommier <marie.a.s45@gmail.com>
2020-08-17 09:19:59 +0200

    created loop

---
+++ JS9-More/script.js
@@ -0,0 +1,8 @@
+var mine = 100;
+while (mine>0) { 
+mine--;
+console.log(mine);
+}
+
+//Ecrivez ici la boucle qui vous permet d'enlever 1 unité d'or de la mine et d'annoncer
+//dans la console combien il reste d'or dans la mine.
