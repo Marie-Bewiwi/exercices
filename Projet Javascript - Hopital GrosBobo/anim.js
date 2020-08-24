1ea0a69d2c35f8d3276e43e7c468166e13e13a32
Sommier <marie.a.s45@gmail.com>
2020-08-24 19:24:54 +0200

    effective buttons w/ description

--- Projet Javascript - Hopital GrosBobo/anim.js
+++ Projet Javascript - Hopital GrosBobo/anim.js
@@ -1,23 +1,23 @@
 let containerbouton = document.getElementById("corpstext");
 let boutons = document.getElementsByClassName('btn');
+let descriptions = document.getElementsByClassName('desc');
 
-for(var i = 0 ; i < boutons.length; i++) {
+for(let i = 0 ; i < boutons.length; i++) {
     boutons[i].addEventListener('click', function () {
-        for (var j = 0; j < boutons.length; j++) {
+        for (let j = 0; j < boutons.length; j++) {
         boutons[j].classList.remove('active');
         }
         this.classList.add('active');
-
+        //la même, pour les descriptions.
+        for (let k=0 ; k < descriptions.length; k++){
+            descriptions[k].classList.remove('shown')
+        }
+        this.nextElementSibling.classList.add('shown'); // on cible le "frère" des boutons, qui sont les descrip.
     });
 }
 
-let description = document.getElementsByClassName('desc');
-for (var k=0 ; k < description.length; k++ ) {
-    boutons[k].addEventListener('click' , function() {
 
-    }
     
+    // QUAND click , display = bloc pour l'actif
+    // clic = change la classe dans le css pour afficher et désafficher les autres
     
-    
-    )
-}
