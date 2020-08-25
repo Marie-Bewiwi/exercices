255aa471cf27b5d07ab386bfb170befeb616ea02
Sommier <marie.a.s45@gmail.com>
2020-08-25 11:52:32 +0200

    finished 4 lvl 2

---
+++ JS_lvl2_4/script.js
@@ -0,0 +1,11 @@
+function addition (nombre1) {
+     let resultat = nombre1+10
+    return resultat
+}
+
+function multiplier (nombre2) {
+    let resultat2 = nombre2*4
+    return resultat2
+}
+
+console.log(addition(multiplier(4))); // le nombre1 devient le résultat de multiplier 
