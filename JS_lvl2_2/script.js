3896f59aa3df19fdca02423f1bc0b483abe23b01
Sommier <marie.a.s45@gmail.com>
2020-08-25 11:25:45 +0200

    JS 2 lvl 2 finished, commented version

---
+++ JS_lvl2_2/script.js
@@ -0,0 +1,5 @@
+function addition (nombre) { // ici, nombre est un paramètre : il est voué à être remplacé quand la fonction est appelée
+   return nombre+nombre
+}
+console.log (addition(3)) // on définit ici la valeur du paramètre défini précédemment
+console.log (addition(5))
