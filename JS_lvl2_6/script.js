f26eef3f85c73661f8d15ff01545a57f0f403790
Sommier <marie.a.s45@gmail.com>
2020-08-26 16:21:18 +0200

    started exercices

---
+++ JS_lvl2_6/script.js
@@ -0,0 +1,6 @@
+let bolsdesoupe = document.getElementsByClassName('soupe')
+let boldesoupe = bolsdesoupe[0].children[2] //je dois créer un indice pour parcourir mes enfants
+console.log(boldesoupe)
+//je dois définir mes autres variables
+
+//je dois créer une boucle afin de parcourir mes enfants avec l'indice
