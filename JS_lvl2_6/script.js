d6a8e24f6e4729928312e9ef60315dd7c6f51d58
Sommier <marie.a.s45@gmail.com>
2020-08-27 21:58:59 +0200

    boire function done

--- JS_lvl2_6/script.js
+++ JS_lvl2_6/script.js
@@ -1,6 +1,19 @@
-let bolsdesoupe = document.getElementsByClassName('soupe')
-let boldesoupe = bolsdesoupe[0].children[2] //je dois créer un indice pour parcourir mes enfants
-console.log(boldesoupe)
-//je dois définir mes autres variables
+let bolsdesoupe = document.querySelectorAll(".soupe p")
+let boutonboire = document.querySelector("#boire")
 
-//je dois créer une boucle afin de parcourir mes enfants avec l'indice
+
+function boire(bol) {
+    bol.innerHTML = bol.innerHTML.replace("plein", "vide")
+}
+
+function onclick () {
+    for(let i = 0; i < bolsdesoupe.length; i++) {
+        let bol = bolsdesoupe[i];
+        if (bol.innerHTML.match("plein")) { // match = https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/String/match 
+            boire(bol)
+            break
+        }
+    }
+}
+boutonboire.addEventListener("click", onclick);
+
