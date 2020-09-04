7774a73417ec7c5ece9c8873c6672f30fc68c6eb
Sommier <marie.a.s45@gmail.com>
2020-09-04 16:08:45 +0200

    first draft done

---
+++ JS_lvl3_Ex5/script.js
@@ -0,0 +1,23 @@
+do {
+age = Number(prompt("Age ?"))}
+while (isNaN(age));
+if (isNaN(age)) {
+    alert("Veuillez entrer un entier")
+}
+else if (age < 18) {
+    alert("vous n'avez pas le droit de venir sur ce site")
+}
+else if (age >= 18 && age <= 50) {
+    alert("Vous êtes majeur mais pas encore senior, la vie est belle")
+}
+else if (age > 50 && age <= 60) {
+    alert("Vous êtes senior mais pas encore retraité")
+}
+else if (age > 60) {
+    alert("Vous êtes retraité, profitez de votre temps libre !")
+}
+
+
+
+
+
