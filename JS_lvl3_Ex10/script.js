23d9ea2e1e4f22e543b9a0aa5c15a92a14c7a7af
Sommier <marie.a.s45@gmail.com>
2020-09-07 15:59:03 +0200

    finished ex 10 + added missing files

---
+++ JS_lvl3_Ex10/script.js
@@ -0,0 +1,13 @@
+let bouton = document.getElementById('bouton1');
+
+
+bouton.addEventListener('click', changebutton);
+
+function changebutton() {
+    if (bouton.value === "Cliquez moi!") {
+        bouton.value = "j'ai été cliqué"
+    }
+ else {
+     bouton.value = "Cliquez moi!"
+ }
+}
