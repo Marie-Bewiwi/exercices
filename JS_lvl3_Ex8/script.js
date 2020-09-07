f598e5b202fa3c9d6ee80f70655b83374d687130
Sommier <marie.a.s45@gmail.com>
2020-09-07 14:22:03 +0200

    fixed date

--- JS_lvl3_Ex8/script.js
+++ JS_lvl3_Ex8/script.js
@@ -1,6 +1,7 @@
 let mybirthday = new Date ("May 4, 2021 16:25:00")
 let today = Date.now()
+let datetoday = Date()
 let jours= Math.floor((mybirthday - today)/1000/86400)
 
-document.write ('<p>Mon anniversaire est le ' + mybirthday  + '</p> <p> Aujourd\'hui nous sommes le ' + today + '</p> <p> Il reste donc ' + jours + ' jours avant mon anniversaire ! ')
+document.write ('<p>Mon anniversaire est le ' + mybirthday  + '</p> <p> Aujourd\'hui nous sommes le ' + datetoday + '</p> <p> Il reste donc ' + jours + ' jours avant mon anniversaire ! ')
 
