4ddefd5133b0a44d745f590011fe9595881c52ef
Sommier <marie.a.s45@gmail.com>
2020-09-07 15:35:47 +0200

    v2 done

--- JS_lvl3_Ex9/script.js
+++ JS_lvl3_Ex9/script.js
@@ -1,11 +1,22 @@
+// code for v1 : as requested in readme doc
 let sum = document.getElementById('summary');
 let btnlireplus = document.getElementById('showmore');
 let fulltext = document.getElementById('full')
 
-btnlireplus.addEventListener('click',showmore)
+btnlireplus.addEventListener('click', showmore)
 
-function showmore () {
-sum.style.display = "none";
-btnlireplus.style.display = "none";
-fulltext.style.display = 'block';
+
+function showmore() {
+    sum.style.display = "none";
+    btnlireplus.style.display = "none";
+    fulltext.style.display = 'block';
 }
+
+// code for v2 : updated 
+
+if (fulltext.textContent.length > 100) {
+    sum.style.display = "block";
+    fulltext.style.display = "none"
+    btnlireplus.style.display = "block"
+}
+
