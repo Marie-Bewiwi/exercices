4816fa6e45c0681db1c91d5cd0a4888a7714ba4a
Sommier <marie.a.s45@gmail.com>
2020-09-08 11:52:47 +0200

    first step of cross done

--- JSBosslvl3_Matrice de formes/script.js
+++ JSBosslvl3_Matrice de formes/script.js
@@ -8,18 +8,26 @@ let ligne3 = document.querySelectorAll('#ligne3 td')
 let ligne4 = document.querySelectorAll('#ligne4 td')
 let ligne5 = document.querySelectorAll('#ligne5 td')
 let lignes = document.querySelectorAll('#ligne1,#ligne2,#ligne4,#ligne5')
-//let colonne3 = document.querySelector('.ligne td:nth-of-type(3)')
+let colonne3 = document.querySelectorAll('.ligne td:nth-of-type(3)')
+let losange = document.querySelector('.losange')
 
 boutonremplir.addEventListener('click', fill)
 boutonvider.addEventListener('click', empty)
-carres.forEach(function (elem,index) { //coucou c'est le bordel
+carres.forEach(function (elem,index) {
     elem.addEventListener("click", function () {
         let colonnes = lignes[index].querySelectorAll('td');
-        for (let k = 0; k < colonnes.length; k++) { //ici ça merde ? 
+        for (let k = 0; k < colonnes.length; k++) { 
             colonnes[k].classList.add('plein')
         }
     });
 });
+losange.addEventListener('click',remplircroix) 
+function remplircroix() {
+for (let l = 0; l < colonne3.length ; l++) {
+        colonne3[l].classList.add('plein')
+    }
+   
+}
 
 function fill() {
     for (let i = 0; i < allforms.length; i++) {
