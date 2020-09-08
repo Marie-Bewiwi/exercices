8e6146571b61cd7d21b934ec48d112d404e98cc7
Sommier <marie.a.s45@gmail.com>
2020-09-08 11:04:08 +0200

    added whitespace

--- JS_lvl3_Ex10/script.js
+++ JS_lvl3_Ex10/script.js
@@ -7,7 +7,7 @@ function changebutton() {
     if (bouton.value === "Cliquez moi!") {
         bouton.value = "j'ai été cliqué"
     }
- else {
-     bouton.value = "Cliquez moi!"
- }
+    else {
+        bouton.value = "Cliquez moi!"
+    }
 }
